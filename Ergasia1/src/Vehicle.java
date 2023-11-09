5478549f13b93a3441cbf869c00087b463622c14
Christos Christodoulou <110767712+ChristosCSaI@users.noreply.github.com>
2023-11-09 19:02:04 +0200

    Update Vehicle.java
    Fixed Vehicle class violations by creating methods for each work

--- Ergasia1/src/Vehicle.java
+++ Ergasia1/src/Vehicle.java
@@ -1,29 +1,24 @@
-// SRP, OCP, DIP Violations
-class Vehicle {
+
+public class Vehicle {
     private String model;
     private String make;
     private int year;
     private boolean isElectric;
 
-    // God Class, Long Method, Feature Envy
-    public void printVehicleDetails() {
-        System.out.println("Model: " + model + ", Make: " + make + ", Year: " + year);
-        if (isElectric) {
-            System.out.println("Electric Vehicle");
-        } else {
-            System.out.println("Non-Electric Vehicle");
-        }
-        if (year > 2015) {
-            System.out.println("Recent model");
-        } else {
-            System.out.println("Old model");
-        }
-        // State Checking
-        if (model.equals("Tesla")) {
-            System.out.println("Luxury vehicle");
-        } else {
-            System.out.println("Regular vehicle");
-        }
+    public String getModel() {
+        return model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public boolean isElectric() {
+        return isElectric;
     }
 }
 
