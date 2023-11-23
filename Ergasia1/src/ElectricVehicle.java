78762305d7f410c8dae13882c23c9317ec19bf9f
Christos Christodoulou <110767712+ChristosCSaI@users.noreply.github.com>
2023-11-23 19:36:50 +0200

    Update ElectricVehicle.java
    Constructor added

--- Ergasia1/src/ElectricVehicle.java
+++ Ergasia1/src/ElectricVehicle.java
@@ -1,7 +1,10 @@
 // Utilize inheritance correctly with meaningful behavior
-class ElectricVehicle extends Vehicle {
+class ElectricVehicle extends Vehicle implements Communication,Navigation {
     // Method specific to electric vehicles
-    public void printElectricDetails() {
-        System.out.println("Electric Vehicle");
+	public ElectricVehicle(String model,String make,int year,boolean isElectric){
+	        super();
     }
-}
+	        public void printElectricDetails() {
+	        	System.out.println("Electric Vehicle");
+    }
+
