a9ee6ecfb9efacc43456a662b29f2530af869bac
Christos Christodoulou <110767712+ChristosCSaI@users.noreply.github.com>
2023-11-23 19:39:20 +0200

    Update Vehicle.java
    Vehicle becomes a superclass

--- Ergasia1/src/Vehicle.java
+++ Ergasia1/src/Vehicle.java
@@ -1,10 +1,15 @@
-
-public class Vehicle {
+class Vehicle {
     private String model;
     private String make;
     private int year;
     private boolean isElectric;
 
+    public Vehicle(String model, String make, int year, boolean isElectric) {
+    	this.model=model;
+    	this.make=make;
+    	this.year=year;
+    	this.isElectric=isElectric;
+    }
     public String getModel() {
         return model;
     }
